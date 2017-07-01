1d733ccbd8a5d9dd38270114454012e48a28c40a
xuning <xuning@eefung.com>
2017-07-02 02:00:11 +0800

    添加单元测试例子

--- src/main/test/com/app/AppTest.java
+++ src/main/test/com/app/AppTest.java
@@ -1,9 +1,26 @@
 package com.app;
 
-/****************************************
- * Copyright (c) xuning.
- * @Auther is xuning on 17-7-2
- ****************************************/
+import com.app.service.TestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(AppStart.class)
+@ActiveProfiles(value = "dev")
 public class AppTest {
+    @Autowired
+    private TestService testService;
     
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
+    @Test
+    public void testInfo(){
+        String res = testService.start();
+        LOGGER.info(res);
+    }
 }
