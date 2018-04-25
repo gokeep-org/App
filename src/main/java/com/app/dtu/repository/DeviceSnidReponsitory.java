4cb4f81bbe78b66e00ac4f4e0dcc2a44e216239b
xuning <xuning@ablecloud.cn>
2018-04-25 13:55:53 +0800

    修改异步方法调用，修复bug'

--- src/main/java/com/app/dtu/repository/DeviceSnidReponsitory.java
+++ src/main/java/com/app/dtu/repository/DeviceSnidReponsitory.java
@@ -2,18 +2,6 @@ package com.app.dtu.repository;
 
 import com.app.dtu.bean.model.DeviceSnid;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.annotation.Async;
-
-import java.util.List;
 
 public interface DeviceSnidReponsitory extends JpaRepository<DeviceSnid, Long> {
-
-    /**
-     * 这里需要实现异步的方法调用，保证中间不被阻塞
-     *
-     * @return
-     */
-    @Async
-    @Override
-    List<DeviceSnid> findAll();
 }
