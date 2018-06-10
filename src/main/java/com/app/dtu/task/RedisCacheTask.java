fd23609ef47f5a80064a6d3d211f292ccffb097c
xuning <xuning@ablecloud.cn>
2018-06-11 04:42:36 +0800

    修改redis缓存配置

--- src/main/java/com/app/dtu/task/RedisCacheTask.java
+++ src/main/java/com/app/dtu/task/RedisCacheTask.java
@@ -1,13 +1,9 @@
 package com.app.dtu.task;
 
-import com.app.dtu.bean.model.PreviousData;
-import com.app.dtu.redis.RedisClient;
 import com.app.dtu.repository.RedisCachePreviousDataReponisory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.CollectionUtils;
-
-import java.util.List;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisCacheTask {
@@ -15,21 +11,25 @@ public class RedisCacheTask {
     @Autowired
     RedisCachePreviousDataReponisory redisCachePreviousDataReponisory;
 
-    RedisClient redisClient;
+    @Autowired
+    StringRedisTemplate redisClient;
     /**
      * 缓存上一条数据到缓存
      */
 //    @Scheduled(cron = DtuConfig.CACHE_PREVIOUS_DATA)
     public void cachePreviousData(){
-        List<PreviousData> previousDataList = redisCachePreviousDataReponisory.findAll();
-        if (CollectionUtils.isEmpty(previousDataList)) {
-            return;
-        }
-        previousDataList.stream().filter(previousData -> {
-//            List<String> data = redisClient.hmget(previousData.getDeviceId(), "warn", "id", "status");
-            return true;
-        }).forEach(previousData -> {
-            redisClient.hmset(previousData.getDeviceId(), previousData.toMap());
-        });
+//        redisClient.
+//        List<PreviousData> previousDataList = redisCachePreviousDataReponisory.findAll();
+//        if (CollectionUtils.isEmpty(previousDataList)) {
+//            return;
+//        }
+//        redisClient.opsForHash().
+//        previousDataList.stream().filter(previousData -> {
+//
+////            List<String> data = redisClient.hmget(previousData.getDeviceId(), "warn", "id", "status");
+//            return true;
+//        }).forEach(previousData -> {
+//            redisClient.hmset(previousData.getDeviceId(), previousData.toMap());
+//        });
     }
 }
